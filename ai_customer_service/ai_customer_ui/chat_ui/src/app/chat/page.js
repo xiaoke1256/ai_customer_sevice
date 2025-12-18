e0124e52a7976c8ec1bc1a5ee8969f62cd56b636
xiaoke_1256 <xiaoke_1256@sina.com>
2025-12-18 16:26:47 +0800

    chart ui,bg

---
+++ ai_customer_service/ai_customer_ui/chat_ui/src/app/chat/page.js
@@ -0,0 +1,21 @@
+'use client';
+
+export default function ChatIndex() {
+    return (
+    <>
+        <div className="flex h-screen flex-col items-center justify-around" 
+            style={{ 
+                backgroundImage: 'url(chat_bg.png)',
+                backgroundSize: "cover",
+                backgroundPosition: "center" 
+            }} 
+        >
+            <div className="h-full mt-5 mb-5 max-w-[1060px] w-full bg-blue-200 opacity-75">
+                <div className="head">
+
+                </div>
+            </div>
+        </div>
+    </>
+    );
+}
