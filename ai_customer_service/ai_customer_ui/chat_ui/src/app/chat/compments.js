7d6b1ee46c73643cf1733b1a15b80bb3872c382c
xiaoke_1256 <xiaoke_1256@sina.com>
2025-12-20 14:10:46 +0800

    sse

--- ai_customer_service/ai_customer_ui/chat_ui/src/app/chat/compments.js
+++ ai_customer_service/ai_customer_ui/chat_ui/src/app/chat/compments.js
@@ -1,33 +1,60 @@
 'use client';
 
 import {ChevronLeftIcon} from '@heroicons/react/24/outline';
-import {Button,Input} from '@heroui/react'
+import {Button,Input} from '@heroui/react';
+import {useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 export function Chat() {
     //从后台获取上次聊天的sessionId
     //从后台获取上次聊天的session内容
-    //若从后台无法获取sessionId，就生成一个新的SessionId
+    //若从后台无法获取sessionId，就生成一个新的sessionId
+    const sessionId = uuid();
+
+    console.log("sessionId:",sessionId);
+
+    const [userPrompt,setUserPrompt] = useState('');
 
     const sendMsg = (e)=>{
-        const source = new EventSource('/openSseChat',
+        console.log('sendMsg ... ');
+        const source = new EventSource(`http://localhost:8080/openSseChat?userPrompt=${userPrompt}`,
             {
                 headers: {
-                    'Authorization': 'Bearer your_token',
-                    'Custom-Header': 'custom_value'
+                    'sessionId': sessionId
                 }
             }
         );
+        source.addEventListener('start', (event) => {
+            console.log('Custom event:', event.data);
+        });
+        source.addEventListener('complete', (event) => {
+            console.log('Custom event:', event.data);
+            source.close();
+        });
         source.onmessage = function(event) { // 当接收到消息时触发此函数
-            console.log('New message:', event.data); // 打印接收到的数据
+            console.log('New message:', event.type,event.data); // 打印接收到的数据
         };
         source.onopen = function(event){
             console.log('onopen:', event);
         }
         source.onerror = function(event){
-
+            console.log('onerror:', event);
+            console.log('event.target.readyState:', event.target.readyState);
+            if (event.target.readyState == EventSource.CLOSED) {
+                console.log('Connection closed');
+                if (event.target.lastEventId) {
+                    console.log('Last event ID:', event.target.lastEventId);
+                }
+                if (event.target.url) {
+                    console.log('SSE URL:', event.target.url);
+                }
+            } else {
+                console.error('EventSource error:', event);
+            }
+            source.close();
         }
         source.onclose = function(event){
-
+            console.log('onclose:', event);
         }
     }
     
@@ -41,7 +68,7 @@ export function Chat() {
 
             </div>
             <div className="foot m-2 flex">
-                <Input className="m-1"></Input>
+                <Input className="m-1" value={userPrompt} onValueChange={setUserPrompt} ></Input>
                 <Button color='primary' className="m-1" onPress={sendMsg}>发送</Button>
             </div>
 
