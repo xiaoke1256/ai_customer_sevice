10f70190eb6a0b47fee137cd2f9eb9f41bda8a24
xiaoke_1256 <xiaoke_1256@sina.com>
2026-01-27 23:18:25 +0800

    测试订单回访功能

--- spring-ai_test/src/test/java/TestAli.java
+++ spring-ai_test/src/test/java/TestAli.java
@@ -53,4 +53,31 @@ public class TestAli {
         String content = result.content();
         System.out.println(content);
     }
+
+    @Test
+    public void testClient2(@Autowired ChatClient.Builder builder){
+        ChatClient client = builder
+                .defaultSystem("""
+                ## 角色定义
+                你是一个智能客服。
+                ## 行为指南
+                订单回访时仅询问是否收到商品，并引导到商品商品评价页面(www.orders.com/product/{productCode}/remark)
+                
+                """)
+                .build();
+        ChatClient.CallResponseSpec result = client.prompt().user("""
+               订单信息如下：
+               { "订单号":"03RAPM37355894M",
+                 "商品代码":"0009100515",
+                 "运费":7.00,
+                 "总金额":15002,
+                 "商户号":"2018100021",
+                 "订单状态":"送货中",
+                 "下单时间":"2025-11-13"
+               }
+               请您进行一下回访。
+                """).call();
+        String content = result.content();
+        System.out.println(content);
+    }
 }
