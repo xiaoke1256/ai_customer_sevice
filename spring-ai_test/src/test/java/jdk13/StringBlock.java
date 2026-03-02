3384e47984565c1b5bfd06279323271587440d6c
xiaoke_1256 <xiaoke_1256@sina.com>
2026-03-02 18:14:49 +0800

    jdk13 test

---
+++ spring-ai_test/src/test/java/jdk13/StringBlock.java
@@ -0,0 +1,19 @@
+package jdk13;
+
+public class StringBlock {
+    public static void main(String[] args) {
+        String s1 = """
+                   1
+                   2
+                   3
+                   4
+                   5
+                   6
+                   7
+                   8
+                   9
+                   10
+                """;
+        System.out.println(s1);
+    }
+}
