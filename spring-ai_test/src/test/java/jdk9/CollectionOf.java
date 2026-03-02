33b7243dcdf2f785bb076d2a6ae1a3a4ee05d973
xiaoke_1256 <xiaoke_1256@sina.com>
2026-03-02 10:14:28 +0800

    jdk9 test

--- spring-ai_test/src/test/java/jdk9/CollectionOf.java
+++ spring-ai_test/src/test/java/jdk9/CollectionOf.java
@@ -25,4 +25,17 @@ public class CollectionOf {
         var map = Map.of("1",1,"2",2,"3",3,"4",4,"5",5);
         System.out.println(map);
     }
+
+    @Test
+    public void streamApi() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 3, 2, 1);
+
+        // takeWhile：取满足条件的元素直到不满足为止
+        System.out.println(numbers.stream().takeWhile(n -> n < 4).toList());
+        // 结果: [1, 2, 3]
+
+        // dropWhile：跳过满足条件的元素
+        System.out.println(numbers.stream().dropWhile(n -> n < 4).toList());
+        // 结果: [4, 3, 2, 1]
+    }
 }
